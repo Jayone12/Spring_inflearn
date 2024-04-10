e9d01f94c1352b8a27d2aa0ec8eaa8ec45257776
bigJay <dolt1202@gmail.com>
2024-04-10 22:01:43 +0900

    fix #6 회원 레포지토리 Bean 수정

--- helloSpring/src/main/java/com/hello/hellospring/SpringConfig.java
+++ helloSpring/src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,22 +1,32 @@
 package com.hello.hellospring;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import com.hello.hellospring.repository.JdbcMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
-import com.hello.hellospring.repository.MemoryMemberRepository;
 import com.hello.hellospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
-	@Bean
-	public MemberService memberService() {
-		return new MemberService(memberRepository());
-	}
-	
-	@Bean
-	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
-	}
+  private DataSource dataSource;
+
+  @Autowired
+  public SpringConfig(DataSource dataSource) {
+    super();
+    this.dataSource = dataSource;
+  }
+
+  @Bean
+  public MemberService memberService() {
+    return new MemberService(memberRepository());
+  }
+
+  @Bean
+  public MemberRepository memberRepository() {
+    // return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
+  }
 }
